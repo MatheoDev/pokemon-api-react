6f0785532dd891b079ea6851d811f5f4861522c5
Matheo Deknuydt <matheodeknuydt@MacBook-Pro-de-Matheo.local>
2022-03-30 10:40:43 +0200

    detail of pokemon adding info

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,5 +1,8 @@
-const Jumbotron = ({text}) => <div className="jumbotron">
+import './jumbotron.css'
+
+const Jumbotron = ({text, img = null}) => <div className="jumbotron">
   <h1 className="text-white fs-3">{text}</h1>
+  {img && <img className="jumbotron__img" src={img} alt={img} />}
 </div>
 
 export default Jumbotron
--- src/pages/Pokemon/Pokemon.js
+++ src/pages/Pokemon/Pokemon.js
@@ -1,10 +1,20 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import Jumbotron from "../../components/Jumbotron/Jumbotron"
 
 const Pokemon = () => {
   const [data, setData] = useState(null)
   const { id } = useParams()
+
+  /**
+   * First litter uppercase for name of pokemon
+   * @param {*} string 
+   * @returns 
+   */
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
@@ -13,11 +23,23 @@ const Pokemon = () => {
       })
       .catch(error => setData(null))
   }, [])
-
+  
   return (
-    <div className="container">
-      {data && data.name}
-    </div>
+    <>
+      {data &&
+        <>
+          <Jumbotron text={`#${data.id} - ${capitalizeFirstLetter(data.name)}`} img={data.sprites.versions['generation-viii'].icons.front_default} />
+          <div className="container mt-4">
+            <div className="container__forme">
+              <h2>Normal/Shiny</h2>
+              <img src={data.sprites.front_default} alt={data.sprites.front_default} />
+              <img src={data.sprites.front_shiny} alt={data.sprites.front_shiny} />
+            </div>
+            {console.log(data)}
+          </div>
+        </>
+      }
+    </>
   )
 }
 
